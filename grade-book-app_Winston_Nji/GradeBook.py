3f47dd4f2bbae531120ebc8c00b816397e8302de
Winston Nji <w.nji1@alustudent.com>
2024-07-22 12:47:00 +0200

    Realised student class and course class wasn't working in the main GradeBook since they were in different folders, so ended up importing code from those two classes and functions to GradeBook

--- grade-book-app_Winston_Nji/GradeBook.py
+++ grade-book-app_Winston_Nji/GradeBook.py
@@ -1,3 +1,39 @@
+#Initializing the different attributes of student detailes by using a class method
+
+class Student:
+    def __init__(self, email, names):
+        self.email = email
+        self.names = names
+        self.courses_registered = []
+        self.GPA = 0.0
+
+#Next step will be to calculate the studenst GPA based on the course he/she registered
+
+    def calculate_GPA(self):
+        # Calculate GPA based on the registered courses
+        if len(self.courses_registered) == 0:
+            self.GPA = 0.0
+        else:
+            total_points = sum(course['grade'] * course['credits'] for course in self.courses_registered)
+            total_credits = sum(course['credits'] for course in self.courses_registered)
+            self.GPA = total_points / total_credits
+
+#Final step for student initizialization will be to Register the student for a course
+
+    def register_for_course(self, course):
+         self.courses_registered.append(course)
+
+#Inizialization of attributes for the student course classes
+
+class Course:
+    def __init__(self, name, trimester, credits):
+        # Initialize the attributes of the Course class
+        self.name = name
+        self.trimester = trimester
+        self.credits = credits
+
+
+
 
 # Initializing the student_list and course_list as empty arrays
 class GradeBook:
@@ -22,3 +58,4 @@ class GradeBook:
         new_course = Course(name, trimester, credits)
         # Append the new course to the course_list
         self.course_list.append(new_course)
+
