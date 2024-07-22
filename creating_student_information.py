227ad76ac61782f126464a69f6758df19ddc59e4
Winston Nji <wnji1@alustudent.com>
2024-07-22 10:16:53 +0000

    more work on studetn information

--- creating_student_information.py
+++ creating_student_information.py
@@ -1,7 +1,18 @@
+#Initializing the different attributes of student detailes by using a class method
+
 class Student:
     def __init__(self, email, names):
-        # Initialize the attributes of the Student class
         self.email = email
         self.names = names
         self.courses_registered = []
         self.GPA = 0.0
+
+#Next step will be to calculate the studenst GPA based on the course he/she registered
+
+    def calculate_GPA(self):
+	if len(self.courses_registered) == 0:
+            self.GPA = 0.0
+        else:
+            total_points = sum(course['grade'] * course['credits'] for course in self.courses_registered)
+            total_credits = sum(ourse['credits'] for course in self.courses_registered)
+            self.GPA = total_points / total_credits
