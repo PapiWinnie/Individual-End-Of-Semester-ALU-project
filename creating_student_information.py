2a61b7bf76ab3d7406794ac61eaf456fc260401c
Winston Nji <w.nji1@alustudent.com>
2024-07-22 12:20:40 +0200

    Finalization of student creation process done

--- creating_student_information.py
+++ creating_student_information.py
@@ -10,9 +10,15 @@ class Student:
 #Next step will be to calculate the studenst GPA based on the course he/she registered
 
     def calculate_GPA(self):
-	if len(self.courses_registered) == 0:
+        # Calculate GPA based on the registered courses
+        if len(self.courses_registered) == 0:
             self.GPA = 0.0
         else:
             total_points = sum(course['grade'] * course['credits'] for course in self.courses_registered)
-            total_credits = sum(ourse['credits'] for course in self.courses_registered)
+            total_credits = sum(course['credits'] for course in self.courses_registered)
             self.GPA = total_points / total_credits
+
+#Final step for student initizialization will be to Register the student for a course
+
+    def register_for_course(self, course):
+         self.courses_registered.append(course)
