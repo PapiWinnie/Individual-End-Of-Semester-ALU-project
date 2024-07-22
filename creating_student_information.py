a8c7b253e8971799c854a33074e1fbb14cc636b7
Winston Nji <wnji1@alustudent.com>
2024-07-22 10:10:00 +0000

    Started student inizialization class

---
+++ creating_student_information.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, email, names):
+        # Initialize the attributes of the Student class
+        self.email = email
+        self.names = names
+        self.courses_registered = []
+        self.GPA = 0.0
